39756a1bea44f2775866469d1616ba81828b8919
Raghav Narang <raghav.narang.con@hotstar.com>
2024-01-24 22:01:02 +0530

    fix: removed unused component

--- app/(shop)/account/page.tsx
+++ app/(shop)/account/page.tsx
@@ -1,10 +1,7 @@
-import LogoutButton from "@/components/auth/logout-button";
-
 const Account = () => {
   return (
     <div>
       <h1>My Account</h1>
-      <LogoutButton />
     </div>
   );
 };
