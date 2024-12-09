7dcc1985d5135f39f4e412e3e3b99d41b61a001a
Raghav Narang <raghav.narang.con@hotstar.com>
2024-12-10 02:21:52 +0530

    fix: Self host v2

--- src/utils/logo.ts
+++ src/utils/logo.ts
@@ -1,13 +1,3 @@
-// @ts-ignore
-import logo from '@/public/logo.png'
-
-export function getLogo() {
-  return logo
-}
-
-export function getLogoURL(local?: boolean) {
-  if (!local && process.env.NEXT_PUBLIC_LOGO_URL) return process.env.NEXT_PUBLIC_LOGO_URL
-
-  const baseURL = process.env.NEXT_PUBLIC_VERCEL_URL!
-  return baseURL + getLogo().src
+export function getLogoURL() {
+  return process.env.NEXT_PUBLIC_LOGO_URL || '';
 }
